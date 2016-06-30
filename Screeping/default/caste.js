26a0df2d282867c69c927b29f5e39ee0be447600
Antony Marshall <lordakkrand@gmail.com>
2016-06-30 11:43:57 +1000

    Updated Caste class

--- Screeping/default/caste.js
+++ Screeping/default/caste.js
@@ -44,3 +44,49 @@ var casteFactory = function(spawn, casteSpecs, levelUpSpecs)
 }
 
 module.exports = casteFactory;
+
+
+class Caste {
+    constructor(bodySpec, upgradeSpec, specOrder) {
+        this.m_bodySpec = bodySpec
+        this.m_upgradeSpec = upgradeSpec
+        this.m_specOrder = {}
+        for (var i in specOrder) {
+            this.m_specOrder[specOrder[i]] = i;
+        }
+    }
+
+    getBodyForEnergy(energyCapacity) {
+        var casteSpecsLength = this.m_bodySpec.length;
+
+        DebugLog('CasteFactory energy ' + energyCapacity + ' original.len ' + casteSpecsLength)
+        var bodyPartIndex = 0;
+        var bodyParts = []
+        while (energyCapacity > 0) {
+            var part = this.m_bodySpec[bodyPartIndex];
+            var partCost = BODYPART_COST[part];
+            DebugLog('attempt part ' + part + ' at cost ' + partCost)
+            if (partCost > energyCapacity) { break; }
+            energyCapacity -= partCost;
+            bodyParts.push(part);
+            DebugLog('added, ' + energyCapacity + ' remaining')
+            bodyPartIndex++;
+            if (bodyPartIndex >= casteSpecsLength) {
+                bodyPartIndex = 0;
+                if (this.m_upgradeSpec) {
+                    this.m_bodySpec = this.m_upgradeSpec;
+                    casteSpecsLength = this.m_bodySpec.length;
+                }
+            }
+        }
+        if (bodyParts.length < this.m_bodySpec.length) {
+            bodyParts = this.m_bodySpec.slice(0);
+        }
+        bodyParts.sort(function(a,b) { return this.m_specOrder[a] - this.m_specOrder[b]); });
+        return bodyParts;
+    }
+    
+    getBodyForSpawn(spawn) {
+        return this.getBodyForEnergy(spawn.room.energyAvailable)
+    }
+};
---
+++ Screeping/default/spawn.js
@@ -0,0 +1,19 @@
+// spawn.js
+
+var logThis = false
+var DebugLog = function(str)
+{
+    if (logThis)
+    {
+        console.log(str)
+    }
+}
+Spawn.prototype.update = function()
+{
+
+}
+
+Spawn.prototype.place = function()
+{
+
+}
